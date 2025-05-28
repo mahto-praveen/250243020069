1c1008cc5c3332b85069241c42918175b8d8f784
Praveen Kumar Mahto <praveenmpk11@gmail.com>
2025-05-28 09:13:42 +0530

    My commit for exam

---
+++ database/Database.sql
@@ -0,0 +1,8 @@
+cid,vname,price,discount,mileage,details
+1,Activa,80000,10,35,"Two wheeler. Sturdy. Mileage 30 km."
+2,Santro,95000,20,20,"Product of Hyundai"
+3,Yamaha,160000,5,70,"Sturdy and good mileage."
+4,i-10,3000000,8,18,"Good small car"
+5,WaganR,250000,10,20,"Good mileage car"
+6,"Hero Splendor",75000,9,65,"Best bike"
+7,"Royal Enfield",190000,5,40,"A royal bike"
